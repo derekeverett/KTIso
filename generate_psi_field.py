35be3aaedb692eb45258108745edcd6945f42f39
derekeverett <derekeverett@gmail.com>
2021-03-20 13:22:46 -0400

    script to generate initial psi profiles

---
+++ generate_psi_field.py
@@ -0,0 +1,32 @@
+import numpy as np #useful for math operations
+
+import gaussian_random_fields as gr
+
+
+def yield_psi(nx, dx, l_corr):
+    alpha = l_corr / dx # the correlation length in units of pixel size
+    Z = gr.gaussian_random_field(alpha = alpha, size = nx)
+    #rescale to [0, 2pi]
+    psi = (2.*np.pi)*(Z - np.min(Z))/np.ptp(Z)
+    return psi
+
+
+def generate_psi_fields(nx, dx, l_corr, n_samples):
+
+    for sample in range(n_samples):
+        psi = yield_psi(nx, dx, l_corr)
+        dirname = 'initial_psi_profiles/'
+        fname = dirname + 'psi_'+str(sample)+'.dat'
+        np.savetxt(fname, psi)
+
+
+def main():
+    nx = 100
+    dx = 0.1
+    l_corr=1.
+    n_samples = 100
+
+    generate_psi_fields(nx, dx, l_corr, n_samples)
+
+if __name__ == '__main__':
+    main()
