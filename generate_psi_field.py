bc65bbb4f03ed22b7a84e120a2079d3015e4a20a
Derek Everett <osu9790@owens-rw01.ten.osc.edu>
2021-03-20 17:57:43 -0400

    add function to generate a single psi field

--- generate_psi_field.py
+++ generate_psi_field.py
@@ -10,23 +10,26 @@ def yield_psi(nx, dx, l_corr):
     psi = (2.*np.pi)*(Z - np.min(Z))/np.ptp(Z)
     return psi
 
-
-def generate_psi_fields(nx, dx, l_corr, n_samples):
-
+def generate_psi_fields_dir(nx, dx, l_corr, n_samples):
     for sample in range(n_samples):
         psi = yield_psi(nx, dx, l_corr)
         dirname = 'initial_psi_profiles/'
         fname = dirname + 'psi_'+str(sample)+'.dat'
         np.savetxt(fname, psi)
 
+def generate_psi_field(nx, dx, l_corr):
+    psi = yield_psi(nx, dx, l_corr)
+    dirname = 'initial_psi_profiles/'
+    fname = dirname + 'psi.dat'
+    np.savetxt(fname, psi)
 
 def main():
-    nx = 100
-    dx = 0.1
+    nx = 121
+    dx = 0.168
     l_corr=1.
     n_samples = 100
 
-    generate_psi_fields(nx, dx, l_corr, n_samples)
+    generate_psi_fields_dir(nx, dx, l_corr, n_samples)
 
 if __name__ == '__main__':
     main()
